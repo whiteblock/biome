88703418241368dc3cc9f5b7d9c4d13b06cc800b
n8wb <vanish172@gmail.com>
2019-09-18 09:25:48 -0700

    update the install script

--- install.sh
+++ install.sh
@@ -2,14 +2,7 @@
 
 #INSTALL DOCKER CREDENTIALS
 
-VERSION=1.5.0
-OS=linux
-ARCH=amd64
-
-curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
-  | tar xz --to-stdout ./docker-credential-gcr \
-  | sudo tee /usr/bin/docker-credential-gcr > /dev/null && sudo chmod +x /usr/bin/docker-credential-gcr
-
+gcloud auth configure-docker
 docker-credential-gcr configure-docker
 
 #CLONE THE BIOME REPO 
